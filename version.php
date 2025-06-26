4a6b3aa124aef8816ec9dd8af8f92fe176de171b
Bas Brands <bas@sonsbeekmedia.nl>
2025-06-26 10:15:31 +0200

    Release 1, ready for user testing

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'theme_mawang';
 $plugin->release      = '1.0';
-$plugin->version      = 2025061800;
+$plugin->version      = 2025062600;
 $plugin->requires     = 2024100700;
 $plugin->supported    = [405, 405];
 $plugin->maturity     = MATURITY_STABLE;
